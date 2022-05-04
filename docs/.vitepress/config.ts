e3578c3df980d41a43fac201cb18a7593cd27826
syy11cn <syy11cn@outlook.com>
2022-05-04 13:19:37 +0800

    chore: update config links

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -8,6 +8,8 @@ async function load() {
       posts: await getPosts('en-US', 'Asia/Shanghai'),
       links: [
         { name: 'GitHub', link: 'https://github.com/syy11cn', icon: '/assets/images/github.svg' },
+        { name: 'Posts', link: '/posts', icon: '/assets/images/post.svg' },
+        { name: 'Links', link: '/links', icon: '/assets/images/link.svg' },
       ]
     }
   })
