ab3ef95bb4ebb0ce04cb16135817dea1e0ea626e
syy11cn <syy11cn@outlook.com>
2022-05-13 23:23:11 +0800

    docs: add resume page

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -12,6 +12,7 @@ async function load() {
         { name: 'GitHub', link: 'https://github.com/syy11cn', icon: '/assets/images/github.svg' },
         { name: 'Posts', link: '/posts', icon: '/assets/images/post.svg' },
         { name: 'Links', link: '/links', icon: '/assets/images/link.svg' },
+        { name: 'Resume', link: '/resume', icon: '/assets/images/resume.svg' },
       ],
       favicon: 'https://shaun-logo.oss-cn-beijing.aliyuncs.com/avatar.jpg',
       googleId: 'G-F3PB7TDT3P'
---
+++ docs/resume.md
@@ -0,0 +1,7 @@
+---
+title: Resume
+---
+
+# Resume
+
+<iframe src="/前端开发-孙轶扬-电子科技大学-软件工程-23届-本科.pdf" width="100%" height="500" frameBorder="0"></iframe>
